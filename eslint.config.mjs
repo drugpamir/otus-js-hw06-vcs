537920a79a07735f59cef0ff9449d56aaab4901f
av <av@mcc.vniiem.ru>
2024-09-04 13:25:58 +0300

    add ignore and rules to eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,7 +6,12 @@ export default [
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   {
-    files: ['**/*.test.js'],
+    files: ['src/**/*.js'],
+    ignores: ['**/*.config.js', '!**/eslint.config.js'],
     ...jest.configs['flat/recommended'],
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-undef': 'error',
+    },
   },
 ];
