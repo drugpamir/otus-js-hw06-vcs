1aba3fc52d90f91c5b240fd08ac0926485ec8d83
av <av@mcc.vniiem.ru>
2024-09-06 18:11:38 +0300

    added tests for task7

--- src/task7.js
+++ src/task7.js
@@ -1,25 +1,33 @@
+export const CLS_INPUT_ELM = "pTextInput";
+export const CLS_BTN_SUBMIT = "btnSubmit";
+export const CLS_P_CONTAINER = "pContainer";
+export const P_TAG = "p";
+
 /**
  * Запуск приложения с формой добавления параграфов
- * @param {Element} appEl
+ * @param {Element} app
  */
-export function runApp(appEl) {
-  appEl.innerHTML = `
-    <input autofocus type="text" required minlength="4" placeholder="Новый элемент" />
-    <button class="btnSubmit" type="submit">Добавить параграф</button>
-    <div class="pContainer"></div>
+export function runApp(app) {
+  app.innerHTML = `
+    <input class="${CLS_INPUT_ELM}" autofocus type="text" required placeholder="Новый элемент" />
+    <button class="${CLS_BTN_SUBMIT}" hidden type="submit">Добавить параграф</button>
+    <div class="${CLS_P_CONTAINER}"></div>
   `;
 
-  const input = appEl.querySelector("input");
+  // const input = app.querySelector('.' + CLS_INPUT_ELM);
+  const input = app.getElementsByClassName(CLS_INPUT_ELM)[0];
   input.addEventListener("input", (ev) => {
-    addButton.hidden = ev.target.value.length === 0;
+    const text = ev.target.value.trim();
+    addButton.hidden = text.length === 0;
   });
 
-  const pContainer = appEl.querySelector(".pContainer");
+  const pContainer = app.getElementsByClassName(CLS_P_CONTAINER)[0];
 
-  const addButton = appEl.querySelector(".btnSubmit");
-  addButton.hidden = true;
+  const addButton = app.getElementsByClassName(CLS_BTN_SUBMIT)[0];
   addButton.addEventListener("click", (ev) => {
-    const pText = input.value;
+    const pText = input.value.trim();
+    if (!pText) return;
+
     input.value = "";
     ev.target.hidden = true;
 
@@ -33,10 +41,10 @@ export function runApp(appEl) {
  * @param {string} pText - текст нового параграфа
  */
 function addParagraph(pContainer, pText) {
-  const p = document.createElement("p");
+  const p = document.createElement(P_TAG);
   p.innerHTML = pText;
   pContainer.append(p);
-  const paragraphs = pContainer.querySelectorAll("p");
+  const paragraphs = pContainer.querySelectorAll(P_TAG);
   if (paragraphs.length > 5) {
     paragraphs[0].remove();
   }
