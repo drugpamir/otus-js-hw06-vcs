d8fd2924db8a37761522648ae52a28a700fb3076
av <av@mcc.vniiem.ru>
2024-09-06 13:29:20 +0300

    Elements creation moved from index.html to js files

---
+++ index.js
@@ -0,0 +1,4 @@
+import { runApp } from "./src/task7.js";
+
+const appEl = document.getElementById("app");
+runApp(appEl);
--- src/task7.js
+++ src/task7.js
@@ -1,21 +1,43 @@
-const paragraphs = document.querySelector("#paragraphs");
+/**
+ * Запуск приложения с формой добавления параграфов
+ * @param {Element} appEl
+ */
+export function runApp(appEl) {
+  appEl.innerHTML = `
+    <input autofocus type="text" required minlength="4" placeholder="Новый элемент" />
+    <button class="btnSubmit" type="submit">Добавить параграф</button>
+    <div class="pContainer"></div>
+  `;
 
-const input = document.querySelector("input");
-input.addEventListener("input", (ev) => {
-  addButton.hidden = ev.target.value.length === 0;
-});
+  const input = appEl.querySelector("input");
+  input.addEventListener("input", (ev) => {
+    addButton.hidden = ev.target.value.length === 0;
+  });
 
-const addButton = document.querySelector("#addButton");
-addButton.hidden = true;
-addButton.addEventListener("click", (ev) => {
-  const inputText = input.value;
-  input.value = "";
-  ev.target.hidden = true;
+  const pContainer = appEl.querySelector(".pContainer");
 
+  const addButton = appEl.querySelector(".btnSubmit");
+  addButton.hidden = true;
+  addButton.addEventListener("click", (ev) => {
+    const pText = input.value;
+    input.value = "";
+    ev.target.hidden = true;
+
+    addParagraph(pContainer, pText);
+  });
+}
+
+/**
+ * Добавляет параграф в контейнер с заданным текстом
+ * @param {Element} pContainer - контейнер, содержащий параграфы
+ * @param {string} pText - текст нового параграфа
+ */
+function addParagraph(pContainer, pText) {
   const p = document.createElement("p");
-  p.innerHTML = inputText;
-  paragraphs.append(p);
-  if (paragraphs.querySelectorAll("p").length > 5) {
-    paragraphs.querySelectorAll("p")[0].remove();
+  p.innerHTML = pText;
+  pContainer.append(p);
+  const paragraphs = pContainer.querySelectorAll("p");
+  if (paragraphs.length > 5) {
+    paragraphs[0].remove();
   }
-});
+}
