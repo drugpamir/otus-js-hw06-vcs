bbed171218e4c5e74c800bb7f7f0bedef85bd24d
drugpamir <toll_aa@mail.ru>
2024-09-08 23:17:54 +0300

    task9 solution and tests

---
+++ src/task9.js
@@ -0,0 +1,105 @@
+export const DIGITS_ACCURACY = 2;
+
+/**
+ * 1.Даны длины трёх сторон треугольника. Определить, является ли треугольник прямоугольным
+ * Проверяется по теореме Пифагора
+ * @param {number} a - первая сторона треугольника
+ * @param {number} b - вторая сторона треугольника
+ * @param {number} c - третья сторона треугольника
+ * @returns {boolean} True в случае прямоугольного треугольника
+ */
+export function isTriangleRight(a, b, c) {
+  const sides = [a, b, c];
+  if (sides.some((val) => val <= 0)) {
+    return false;
+  }
+  const hypotenuse = Math.max(...sides);
+  const catetus = sides.filter((val) => val != hypotenuse);
+  return hypotenuse ** 2 === catetus.reduce((acc, val) => acc + val ** 2, 0);
+}
+
+/**
+ * 2.Пользователь вводит число R. Написать программу, которая выведет
+ * в консоль длину окружности и площадь круга с радиусом R
+ * @returns
+ */
+export function printCircleProperties() {
+  const inputText = prompt("Введите радиус окружности", "10");
+  const radius = +inputText;
+  let resultMessage;
+  if (Number.isFinite(radius)) {
+    resultMessage = `${getCircleLength(radius).toFixed(DIGITS_ACCURACY)} ${getCircleArea(radius).toFixed(DIGITS_ACCURACY)}`;
+  } else {
+    resultMessage = "Ошибка. Радиус должен быть числом. Введено: " + inputText;
+  }
+  console.log(resultMessage);
+  return resultMessage;
+}
+/**
+ * Возвращает длину окружности, вычисленную из радиуса
+ * @param {number} radius - радиус окружности
+ * @returns {number} - Длина окружнсоти
+ */
+export const getCircleLength = (radius) => 2 * Math.PI * radius;
+/**
+ * Возвращает площадь окружности, вычисленную из радиуса
+ * @param {number} radius - радиус окружности
+ * @returns {number} - Площадь окружнсоти
+ */
+export const getCircleArea = (radius) => Math.PI * radius ** 2;
+
+/**
+ * Пользователь вводит числа a, b и c. Написать программу,
+ * выводящую корни квадратного уравнения с коэффициентами a, b и c.
+ */
+export function printQuadratiсEquationSolutions() {
+  const inputText = prompt(
+    "Введите коэффициенты а, b и с квадратного уравнения через пробел или запятую",
+    "1, -2, 1"
+  );
+  let resultMessage;
+  const coeffs = inputText.split(/[\s,;]/);
+  const [a, b, c] = coeffs;
+  if (coeffs.length != 3) {
+    resultMessage =
+      "Ошибка. Необходимо ввести 3 числа, разделённые пробелом или запятой";
+  } else {
+    const solutions = getQuadratiсEquationSolutions(...coeffs);
+    const equation = `${a == 1}x + ${b}x + ${c} = 0`;
+    if (solutions.length == 0) {
+      resultMessage = `Квадратное уравнение ${equation} не имеет вещественных корней`;
+    } else if (solutions.length == 1) {
+      resultMessage = `Корень квадратного уравнения ${equation}: ${solutions}`;
+    } else {
+      resultMessage = `Корни квадратного уравнения ${equation}: ${solutions}`;
+    }
+  }
+  console.log(resultMessage);
+  return resultMessage;
+}
+
+// function printQuadratiсEquation(a, b, c) {
+//   const aStr = a == 1 ? "" : a == -1 ? "-" : a;
+//   const bStr = b == 1 ? " + " : b == -1 ? " - " : b > 0 ? ` + ${b}`;
+//   return `${aStr}x + ${bStr}x + ${c} = 0`;
+// }
+
+/**
+ * Возвращает массив корней квадратного уравнения по его конффициентам
+ * @param {number} a
+ * @param {number} b
+ * @param {number} c
+ * @returns {Array}
+ */
+export function getQuadratiсEquationSolutions(a, b, c) {
+  const discriminant = b ** 2 - 4 * a * c;
+  if (discriminant < 0) {
+    return [];
+  } else if (discriminant === 0) {
+    return [-b / 2 / a];
+  } else
+    return [
+      (-b - Math.sqrt(discriminant)) / 2 / a,
+      (-b + Math.sqrt(discriminant)) / 2 / a,
+    ];
+}
---
+++ src/task9.test.js
@@ -0,0 +1,72 @@
+import {
+  DIGITS_ACCURACY,
+  isTriangleRight,
+  printCircleProperties,
+  getCircleLength,
+  getCircleArea,
+  printQuadratiсEquationSolutions,
+  getQuadratiсEquationSolutions,
+} from "./task9";
+
+describe("isTriangleRight", () => {
+  it("returns true for right triangle sizes (parameters in any sequence)", () => {
+    expect(isTriangleRight(3, 4, 5)).toBeTruthy();
+    expect(isTriangleRight(5, 4, 3)).toBeTruthy();
+    expect(isTriangleRight(4, 5, 3)).toBeTruthy();
+  });
+
+  it("returns false for non-right triangle sizes", () => {
+    expect(isTriangleRight(3, 4, 6)).toBeFalsy();
+    expect(isTriangleRight(4, 4, 4)).toBeFalsy();
+  });
+
+  it("returns false if any size <= 0", () => {
+    expect(isTriangleRight(0, 0, 0)).toBeFalsy();
+    expect(isTriangleRight(-3, -4, -5)).toBeFalsy();
+  });
+});
+
+describe("circle function", () => {
+  it("getCircleLength returns length of circle depends on radius", () => {
+    expect(getCircleLength(10).toFixed(DIGITS_ACCURACY)).toBe("62.83");
+    expect(getCircleLength(100).toFixed(DIGITS_ACCURACY)).toBe("628.32");
+  });
+
+  it("getCircleArea returns area of circle depends on radius", () => {
+    expect(getCircleArea(10).toFixed(DIGITS_ACCURACY)).toBe("314.16");
+    expect(getCircleArea(100).toFixed(DIGITS_ACCURACY)).toBe("31415.93");
+  });
+
+  it("printCircleProperties returns circle length and area depends on radius", () => {
+    jest.spyOn(window, "prompt").mockReturnValue("10");
+    expect(printCircleProperties()).toEqual(expect.stringContaining("62.83"));
+    expect(printCircleProperties()).toEqual(expect.stringContaining("314.16"));
+  });
+});
+
+describe("quadratic equation solving function", () => {
+  it("returns 2 solutions for D > 0", () => {
+    expect(getQuadratiсEquationSolutions(1, 5, 6)).toEqual([-3, -2]);
+    expect(getQuadratiсEquationSolutions(1, -1, -6)).toEqual([-2, 3]);
+  });
+
+  it("returns 1 solution for D = 0", () => {
+    expect(getQuadratiсEquationSolutions(1, -2, 1)).toEqual([1]);
+    expect(getQuadratiсEquationSolutions(1, -4, 4)).toEqual([2]);
+  });
+
+  it("returns empty array for D < 0", () => {
+    expect(getQuadratiсEquationSolutions(1, 1, 1)).toEqual([]);
+    expect(getQuadratiсEquationSolutions(1, 2, 4)).toEqual([]);
+  });
+
+  it("printQuadratiсEquationSolutions returns solutions of quadratic equation", () => {
+    jest.spyOn(window, "prompt").mockReturnValue("1 5 6");
+    expect(printQuadratiсEquationSolutions()).toEqual(
+      expect.stringContaining("-3")
+    );
+    expect(printQuadratiсEquationSolutions()).toEqual(
+      expect.stringContaining("-2")
+    );
+  });
+});
